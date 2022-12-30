90359d3f1e150e7e9c5d65529dcd3fd5fdf231ad
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-12-30 14:46:16 +0100

    Add width props and defaultProps values

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,26 +9,27 @@ import PropTypes from 'prop-types';
  * @param {Function} trigger A trigger for turning off the modal
  * @param {String} title The modal title
  * @param message The modal message (can be DOM elements)
+ * @param {Number} width The modal width
  * @returns Custom Modal
  */
-export const Modal = ({ isShown, trigger, title, message }) => {
-  const closeModal = () => trigger(false);
+export const Modal = (props) => {
+  const closeModal = () => props.trigger(false);
 
   return (
     <>
-      {isShown && (
+      {props.isShown && (
         <div className="modal">
-          <div className="modal-content">
+          <div className="modal-content" style={{ width: `${props.width}%` }}>
             <div className="modal-header">
               <div className="modal-title">
-                <h3>{title}</h3>
+                <h3>{props.title}</h3>
               </div>
               <span onClick={closeModal} className="modal-close">
                 &times;
               </span>
             </div>
             <div className="modal-message">
-              <span>{message}</span>
+              <span>{props.message}</span>
             </div>
           </div>
         </div>
@@ -37,9 +38,16 @@ export const Modal = ({ isShown, trigger, title, message }) => {
   );
 };
 
+Modal.defaultProps = {
+  title: 'Your modal title',
+  message: 'Your modal message',
+  width: 27,
+};
+
 Modal.propTypes = {
   isShown: PropTypes.bool.isRequired,
   trigger: PropTypes.func.isRequired,
-  title: PropTypes.string.isRequired,
-  message: PropTypes.any.isRequired,
+  title: PropTypes.string,
+  message: PropTypes.any,
+  width: PropTypes.number,
 };
